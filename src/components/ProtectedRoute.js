cd699c97a5f754057f923f2b500b2cc9caaecc1a
ChekinVR <albertanimations@icloud.com>
2023-08-13 21:01:22 -0700

    Paso del contexto correcto --Lo unico que no sirve es el contexto del user info

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,17 +1,35 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/authContext";
-import {
-  auth,
-  getUserInfo,
-  registerNewUser,
-  userExist,
-} from "../firebase/firebase";
+import { registerNewUser } from "../firebase/firebase";
+
+async function HandleDoc(uid, tmpuser) {
+  await registerNewUser({
+    uid: uid,
+    displayName: tmpuser.displayName,
+    name: "",
+    lastname: "",
+    profilePicture: "",
+    username: "",
+    mail: tmpuser.email,
+    birthday: "",
+    gender: "",
+    address: "",
+    password: tmpuser.password,
+    phoneNumber: "",
+    modDevice: "",
+    processCompleted: false,
+  });
+}
 
 export function ProtectedRoute({ children }) {
-  const { user, loading } = useAuth();
+  const { userInfo, tmpUser, user, loading } = useAuth();
 
   if (loading) {
     return <h1>loading...</h1>;
+  }
+
+  if (userInfo === null) {
+    HandleDoc(user.uid, tmpUser);
   }
 
   if (!user) return <Navigate to="/login" />;
